025ef45198cb09469844c6e8f7cae0ebd5abd7cc
The Pffy Authors <50888935+pffy@users.noreply.github.com>
2022-05-18 20:25:09 -0700

    Update test1.js

--- tests/test1.js
+++ tests/test1.js
@@ -2,12 +2,10 @@
 /*
  * src      : test1.js
  * job      : tests for the generic object
- * git      : https://github.com/pffy/obj-ts
+ * git      : https://github.com/pffy/tmidx-ts
  * author   : The Pffy Authors https://pffy.dev
  * license  : https://opensource.org/licenses/MIT
  */
-const { SomeObject } = require('../.');
+const { ?? } = require('../.');
 
-console.log('hello-world?, %s, %s',
-  SomeObject.derp.key,
-  SomeObject.derp.value);
+// todo: add basic test
